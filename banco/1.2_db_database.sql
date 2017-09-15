16ba6b20f79914b07427840343b78c325d17337a
smsantana <saul.ms@hotmail.com>
2017-09-15 14:36:16 -0300

    Create 1.2_db_database.sql

---
+++ banco/1.2_db_database.sql
@@ -0,0 +1,13 @@
+/ * Comentário de linha única * / 
+CREATE TABLE test_user ( 
+  nome VARCHAR ( 25 ) NOT NULL , 
+  PRIMARY KEY ( nome ) );
+
+
+/ *
+Multi-line
+comente
+* /
+
+- Marcador de posição 
+INSERT INTO $ { tableName } ( name ) VALUES ( 'Mr. T' );  
